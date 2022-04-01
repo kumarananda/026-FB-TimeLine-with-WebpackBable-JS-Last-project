b8c7b4767d75e8361d2babc686dfe741bc354c47
kumarananda <coderanandakumar@gmail.com>
2022-04-02 02:58:10 +0600

    html-BS-modal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./index.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/js/dist/modal";
 
+import 'boxicons/css/boxicons.min.css';
+import "./index.css"
+import "./main.scss"
+import "font-awesome/css/font-awesome.min.css";
+
+import ananda from './img/901fqxtw.bmp'
+
+
+const user_photo = document.getElementById('userMPic');
+// user_photo.setAttribute('src', 'https://scontent.fmaa1-1.fna.fbcdn.net/v/t1.6435-1/160980436_2873645602899195_291559288417585381_n.jpg?stp=dst-jpg_p200x200&_nc_cat=110&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeG0Kh_y38OxYEwEtOJruOL2Gkkuz9CX2b0aSS7P0JfZvd-IMxXnZ-64HpuMeCeFp-u-Sm2yNSJM8dQTFSjH5lVt&_nc_ohc=5qBH-YXvldMAX_fkIMi&_nc_ht=scontent.fmaa1-1.fna&oh=00_AT_WstQX3YSmtJN1qpOOwXaLntJ7f8X25Z-jvn2itred4w&oe=626B889D')
+user_photo.setAttribute('src', ananda)
+const userTPic = document.getElementById('userTPic');
+userTPic.setAttribute('src', ananda);
+
+// const post_container = document.getElementById('post_container');
--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
                 use : ['style-loader', 'css-loader', 'sass-loader'] 
             },
             {
-                test : /\.(jpg|jpeg|png|gif|svg|webp)$/i,
+                test : /\.(jpg|jpeg|png|gif|svg|webp|bmp)$/i,
                 type : 'asset/resource' 
             }
         ]
